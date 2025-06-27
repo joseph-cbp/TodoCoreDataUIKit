c4766862a61fd982e307ec36935a625b43386d50
Joseph Bezerra <joseph.carlosx@gmail.com>
2025-06-27 10:40:11 -0400

    Initial Commit

---
+++ TodoCoreDataUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoCoreDataUIKit
+//
+//  Created by Joseph Pereira on 27/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
