9747452f90446809d69d9955e39b4a4eebf0d133
Joseph Bezerra <joseph.carlosx@gmail.com>
2025-06-27 14:15:48 -0400

    chore: create delegate and implements

--- TodoCoreDataUIKit/Source/Screen/TodoList/TodoListViewModel.swift
+++ TodoCoreDataUIKit/Source/Screen/TodoList/TodoListViewModel.swift
@@ -8,6 +8,39 @@
 import Foundation
 import CoreData
 
-struct TodoListViewModel {
+class TodoListViewModel {
     var context: NSManagedObjectContext
+    var delegate: TodoListViewModelDelegate?
+    private(set) var todoItems: [TodoItem] = []
+    
+    init(context: NSManagedObjectContext){
+        self.context = context
+    }
+    
+    
+    func fetchTodos() {
+        let fetchRequest = TodoItem.fetchRequest()
+        do{
+            let itens: [TodoItem] = try context.fetch(fetchRequest)
+            self.todoItems = itens
+            delegate?.didUpdateTodoList()
+            print("📦 \(todoItems.count) itens carregados do Core Data.")
+            dump(todoItems[0].detail)
+        }catch{
+            print(error.localizedDescription)
+        }
+    }
+    
+    func saveTodo(title: String, description: String){
+        let todo = TodoItem(context: context)
+        todo.title = title
+        todo.detail = description
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context: \(error.localizedDescription)")
+        }
+    }
+    
 }
---
+++ TodoCoreDataUIKit/Source/Screen/TodoList/TodoListViewModelDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  TodoListViewModelDelegate.swift
+//  TodoCoreDataUIKit
+//
+//  Created by Joseph Pereira on 27/06/25.
+//
+
+protocol TodoListViewModelDelegate {
+    func didUpdateTodoList()
+}
