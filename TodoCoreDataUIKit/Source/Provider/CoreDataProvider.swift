6b80f596b27760dd70e5925e300de7e7d4fd9b53
Joseph Bezerra <joseph.carlosx@gmail.com>
2025-06-27 14:16:32 -0400

    chore: create mock instance

--- TodoCoreDataUIKit/Source/Provider/CoreDataProvider.swift
+++ TodoCoreDataUIKit/Source/Provider/CoreDataProvider.swift
@@ -14,6 +14,27 @@ class CoreDataProvider: ProviderProtocol {
         container.viewContext
     }
     
+    static var preview: CoreDataProvider {
+        let provider = CoreDataProvider(inMemory: true)
+        let context = provider.context
+        
+        let todo1 = TodoItem(context: context)
+        todo1.title = "Buy milk"
+        todo1.detail = "Buy some milk from the dairy"
+        
+        let todo2 = TodoItem(context: context)
+        todo2.title = "Learn SwiftUI"
+        todo2.detail = "Read the documentation"
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context: \(error.localizedDescription)")
+        }
+        
+        return provider
+    }
+    
     init(inMemory: Bool){
         container = NSPersistentContainer(name: "TodoCoreDataUIKit")
         if inMemory {
