05a7f5d4219117f422f98b1ec1e59fd3fa419b69
Joseph Bezerra <joseph.carlosx@gmail.com>
2025-07-07 14:41:25 -0400

    chore: Create Coordinator

---
+++ TodoCoreDataUIKit/Source/Utils/FlowController.swift
@@ -0,0 +1,43 @@
+//
+//  FlowController.swift
+//  TodoCoreDataUIKit
+//
+//  Created by Joseph Pereira on 07/07/25.
+//
+
+import UIKit
+import CoreData
+
+class FlowController {
+    private var navigation: UINavigationController?
+    var provider: ProviderProtocol?
+    
+    
+    func start() -> UIViewController? {
+        // TODO: Jogar para uma Factory
+        let startViewController = TodoListViewController(flow: self)
+        let worker = TodoListWorker(coreDataProvider: provider ?? CoreDataProvider(inMemory: false))
+        let presenter = TodoListPresenter()
+        let interactor = TodoListInteractor()
+        presenter.viewController = startViewController
+        interactor.presenter = presenter
+        interactor.worker = worker
+        startViewController.interactor = interactor
+        
+        self.navigation = UINavigationController(rootViewController: startViewController)
+        return navigation
+    }
+    
+    func back() {
+        navigation?.dismiss(animated: true)
+    }
+}
+
+extension FlowController: TodoListFlowDelegate {
+    func presentNewTodoItem(interactor: TodoListInteractor) {
+        let newTask = NewTaskViewController()
+        newTask.interactor = interactor
+        newTask.flow = self
+        self.navigation?.present(newTask, animated: true)
+    }
+}
