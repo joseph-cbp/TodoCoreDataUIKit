c30276bb592c761c88b2d8e7f4e7c9ebee44ffa6
Joseph Bezerra <joseph.carlosx@gmail.com>
2025-07-14 11:25:13 -0400

    chore: update NewTaskViewController to add and update tasks

--- TodoCoreDataUIKit/Source/Screen/NewTask/NewTaskViewController.swift
+++ TodoCoreDataUIKit/Source/Screen/NewTask/NewTaskViewController.swift
@@ -7,10 +7,29 @@
 
 import UIKit
 
+enum TaskMode {
+    case add
+    case update(TodoItemDomain)
+}
+
 class NewTaskViewController: UIViewController {
     
     weak var flow: FlowController?
     weak var interactor: TodoListInteractor?
+    private var mode: TaskMode
+    
+    init(flow: FlowController? = nil, interactor: TodoListInteractor? = nil, mode: TaskMode) {
+        self.flow = flow
+        self.interactor = interactor
+        self.mode = mode
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
         
     private let titleField: UITextField = {
         let textField = UITextField()
@@ -32,13 +51,12 @@ class NewTaskViewController: UIViewController {
         return textField
     }()
     
-    private let addButton: UIButton = {
+    private let actionButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Add Task", for: .normal)
         button.backgroundColor = .systemBlue
         button.layer.cornerRadius = 12
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(createTask), for: .touchUpInside)
+        button.addTarget(self, action: #selector(handleAction), for: .touchUpInside)
         
         return button
     }()
@@ -58,10 +76,28 @@ class NewTaskViewController: UIViewController {
     
     private func setupUI() {
         view.addSubview(formStack)
-        view.addSubview(addButton)
-        view.backgroundColor = .red
-        
+        view.addSubview(actionButton)
+        view.backgroundColor = .gray
+        configureMode()
         setupConstraints()
+    }
+    
+    private func configureMode() {
+        switch self.mode {
+        case .add:
+            actionButton.setTitle("Add Task", for: .normal)
+            actionButton.backgroundColor = .systemGreen
+            
+        case .update(let todo):
+            actionButton.setTitle("Update", for: .normal)
+            actionButton.backgroundColor = .systemBlue
+            populateFields(with: todo)
+        }
+    }
+    
+    private func populateFields(with todo: TodoItemDomain){
+        self.titleField.text = todo.title
+        self.detailField.text = todo.detail
     }
     
     private func setupConstraints() {
@@ -70,20 +106,41 @@ class NewTaskViewController: UIViewController {
             formStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
             formStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
             
-            addButton.topAnchor.constraint(equalTo: formStack.bottomAnchor, constant: 24),
-            addButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
-            addButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
-            addButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -12)
+            actionButton.topAnchor.constraint(equalTo: formStack.bottomAnchor, constant: 24),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -12)
         ])
             
     }
     
     @objc
-    func createTask() {
-        let todo = TodoItemDomain(title: titleField.text ?? "", detail: detailField.text ?? "", createdAt: "")
+    func handleAction() {
+        guard let title = titleField.text, !title.isEmpty else {
+            print("Title cannot be empty")
+            return
+        }
+        switch self.mode {
+        case .add:
+            createAction()
+        case .update(let todoItemDomain):
+            dump(todoItemDomain)
+            updateAction(at: todoItemDomain)
+        }
+    }
+    
+    private func createAction(){
+        let todo = TodoItemDomain(id: nil, title: titleField.text ?? "", detail: detailField.text ?? "", createdAt: "")
+        
         interactor?.addTask(task: todo)
         flow?.back()
-        
-        
+    }
+    
+    private func updateAction(at todo: TodoItemDomain){
+        var todoToUpdate = todo
+        todoToUpdate.title = titleField.text ?? ""
+        todoToUpdate.detail = detailField.text ?? ""
+        interactor?.updateTask(task: todoToUpdate)
+        flow?.back()
     }
 }
